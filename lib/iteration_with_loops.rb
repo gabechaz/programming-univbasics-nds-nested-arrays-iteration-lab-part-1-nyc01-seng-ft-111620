83e12c3dbcbdbd73f6f0ac9bc5972831bf7adc05
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-10-21 17:37:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,4 @@
-
-
 def find_even_values(src)
-
   row_index = 0
   while row_index < src.count do
     element_index = 0
@@ -9,7 +6,7 @@ def find_even_values(src)
       if src[row_index][element_index].even?
         p src[row_index][element_index]
       end
-      element_index =+ 1
+      element_index += 1
     end
     row_index += 1
   end
